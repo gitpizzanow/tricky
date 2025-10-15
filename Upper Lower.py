bf88ff8017a9b2151b19ccbd8a7ad61b302eb622
Ziad \ Lyudmira Segfaultovna <sahraouitarikziad@gmail.com>
2025-10-15 11:20:54 +0100

    Create Upper Lower.py

---
+++ Upper Lower.py
@@ -0,0 +1,35 @@
+
+# def Upper(word : str) ->bool :
+#     return [True for w in word if w.isupper() == True ][0]
+    
+# def Lpper(word : str) ->bool :
+#     return [True for w in word if w.islower() == True ][0]
+
+
+# Upper(
+#     "Hello"
+# )
+
+"""
+to avoid repetition use just pass function as argumet
+# define class first
+"""
+
+def Check(
+    word : str , check_func
+):
+    return [True for w in word if check_func(w) == True ][0]
+
+
+# print(Check(
+#     "Hello",
+#     str.isupper
+# ))
+
+print(
+    # str.isupper(
+    #     "H"
+    # )
+
+    "H".isupper()
+)
